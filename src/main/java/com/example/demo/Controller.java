1e70c7fa07e39baf6691825c0f3f6b46e19a60d7
krulik40000 <44998732+krulik40000@users.noreply.github.com>
2019-12-03 18:12:54 +0100

    Update Controller.java

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -8,6 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
     @RequestMapping("/getDetails/{someString}")
     public Model getDetailsAboutString(@PathVariable String someString) {
-
+        Model response = new Model(someString);
+        response.setNumberOfLowerLetters(someString.chars().filter(Character::isLowerCase).count());
+        response.setNumberOfUpperLetters(someString.chars().filter(Character::isUpperCase).count());
+        response.setNumberOfNumbers(someString.chars().filter(Character::isDigit).count());
+        response.setNumberOfSpecialCharacters(someString.length()-response.getNumberOfLowerLetters()-response.getNumberOfNumbers()-response.getNumberOfUpperLetters());
+        return response;
     }
 }
